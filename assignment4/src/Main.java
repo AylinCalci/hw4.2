2f76065d0766e4bcf4df3695548b215c94b2b195
AylinCalci <aylincalci@hotmail.com>
2021-05-05 01:32:49 +0300

    hw4.2

---
+++ assignment4/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+import Adapter.MernisServiceAdapter;
+import Concrete.BaseCustomerManager;
+
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Customer customer = new Customer(1, "deneme", "lol", new Date(2000, 10, 4), "37018041374");
+		BaseCustomerManager customer = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customer.add(new Customer(1, "Aylin", "Calçý", new Date(1999, 9, 9), "99999999999"));
+	}
+
+}
